3abc0bd9e698b514fbceb32128e8a6f41ea8692f
Alex <99472724+AlexPol1ak@users.noreply.github.com>
2024-12-15 18:02:34 +0300

    Добавьте файлы проекта.

---
+++ dllmain.h
@@ -0,0 +1,10 @@
+﻿// dllmain.h: объявление класса модуля.
+
+class CGraphActiveXModule : public ATL::CAtlDllModuleT< CGraphActiveXModule >
+{
+public :
+	DECLARE_LIBID(LIBID_GraphActiveXLib)
+	DECLARE_REGISTRY_APPID_RESOURCEID(IDR_GRAPHACTIVEX, "{60818e62-e786-4da7-b7d0-dfe0b3997ad9}")
+};
+
+extern class CGraphActiveXModule _AtlModule;
